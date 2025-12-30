f33234379d08378f13abad078f27eac23b23b5ec
Philip Steingrüber <philip.steingruber@gmail.com>
2025-12-30 22:58:23 +0100

    Changed base font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,16 @@ import { TRPCProvider } from "@/trpc/client";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans, Lora } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const josefinSans = Josefin_Sans({
+  variable: "--font-josefin-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const lora = Lora({
+  variable: "--font-lora",
   subsets: ["latin"],
 });
 
@@ -32,7 +32,7 @@ export default function RootLayout({
       <ClerkProvider>
         <html lang="en" suppressHydrationWarning>
           <body
-            className={`${geistSans.variable} ${geistMono.variable} flex h-full w-full flex-col antialiased`}
+            className={`${josefinSans.variable} ${lora.variable} flex h-full w-full flex-col antialiased`}
             suppressHydrationWarning
           >
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
