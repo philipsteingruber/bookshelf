435513064aff2c065216d423a53754c5396b8ef0
Philip Steingrüber <philip.steingruber@gmail.com>
2025-12-31 12:12:19 +0100

    Renamed getUser procedure

--- src/trpc/routers/user.ts
+++ src/trpc/routers/user.ts
@@ -2,7 +2,7 @@ import z from "zod";
 import { authedProcedure, createTRPCRouter } from "../init";
 
 export const userRouter = createTRPCRouter({
-  getUser: authedProcedure
+  getUserByClerkId: authedProcedure
     .input(z.string().min(1))
     .query(async ({ ctx, input: clerkId }) => {
       const user = await ctx.db.user.findUniqueOrThrow({
