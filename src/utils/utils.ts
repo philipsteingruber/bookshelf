85d54355b7a9c8e143aaacfc6cb6a8c33c44cd91
Philip Steingrüber <philip.steingruber@gmail.com>
2026-01-06 22:38:58 +0100

    Changes to prevent flickering on hover

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,33 +4,28 @@ export const parseReadStatus = (readStatus: ReadStatus) => {
   switch (readStatus) {
     case "TO_READ":
       return "To Read";
-      break;
     case "READING":
       return "Reading";
-      break;
     case "READ":
       return "Finished";
-      break;
     case "DNF":
       return "DNF";
-      break;
     case "READ_NEXT":
       return "Read Next";
-      break;
   }
 };
 
 export const getStatusButtonStyle = (readStatus: ReadStatus) => {
   switch (readStatus) {
     case "TO_READ":
-      return "bg-gradient-to-r from-orange-400 to-orange-700 hover:from-orange-400/80 hover:to-orange-700/80 text-white hover:text-neutral-200";
+      return "bg-gradient-to-r from-orange-400 to-orange-700 hover:from-orange-500 hover:to-orange-800 text-white";
     case "READING":
-      return "bg-gradient-to-r from-blue-500 to-blue-800 hover:from-blue-500/80 hover:to-blue-800/80 text-white hover:text-neutral-200";
+      return "bg-gradient-to-r from-blue-500 to-blue-800 hover:from-blue-600 hover:to-blue-900 text-white";
     case "READ":
-      return "bg-gradient-to-r from-green-500 to-green-800 hover:from-green-500/80 hover:to-green-800/80 text-white hover:text-neutral-200";
+      return "bg-gradient-to-r from-green-500 to-green-800 hover:from-green-600 hover:to-green-900 text-white";
     case "DNF":
-      return "bg-gradient-to-r from-red-500 to-red-800 hover:from-red-500/80 hover:to-red-800/80 text-white hover:text-neutral-200";
+      return "bg-gradient-to-r from-red-500 to-red-800 hover:from-red-600 hover:to-red-900 text-white";
     case "READ_NEXT":
-      return "bg-gradient-to-r from-purple-400 to-purple-700 hover:from-purple-400/80 hover:to-purple-700/80 text-white hover:text-neutral-200";
+      return "bg-gradient-to-r from-purple-400 to-purple-700 hover:from-purple-500 hover:to-purple-800 text-white";
   }
 };
