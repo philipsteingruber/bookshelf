5a1228fd22a7994fe90f113001a8294d5c397c31
Philip Steingrüber <philip.steingruber@gmail.com>
2026-01-07 18:52:56 +0100

    Added pino for logging

---
+++ src/lib/logger.ts
@@ -0,0 +1,9 @@
+import pino from "pino";
+
+export const logger = pino({
+  level: "info",
+  transport:
+    process.env.NODE_ENV !== "production"
+      ? { target: "pino-pretty" }
+      : undefined,
+});
