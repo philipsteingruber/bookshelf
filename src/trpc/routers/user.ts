42812cfcd43b21ceaf06db8f84331c73eab73829
Philip Steingrüber <philip.steingruber@gmail.com>
2026-01-07 14:12:31 +0100

    refactor(user.ts): simplify getUserByClerkId procedure to directly return currentUser instead of querying the database by clerkId

--- src/trpc/routers/user.ts
+++ src/trpc/routers/user.ts
@@ -1,14 +1,7 @@
-import z from "zod";
 import { authedProcedure, createTRPCRouter } from "../init";
 
 export const userRouter = createTRPCRouter({
-  getUserByClerkId: authedProcedure
-    .input(z.string().min(1))
-    .query(async ({ ctx, input: clerkId }) => {
-      const user = await ctx.db.user.findUniqueOrThrow({
-        where: { clerkId },
-      });
-
-      return { user };
-    }),
+  getUserByClerkId: authedProcedure.query(async ({ ctx }) => {
+    return { user: ctx.currentUser };
+  }),
 });
