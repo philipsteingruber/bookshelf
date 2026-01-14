4d6dcf59ec86dd980c6ffaa47f33b9abf9d0e05f
Philip Steingrüber <philip.steingruber@gmail.com>
2026-01-14 22:08:17 +0100

    style(book-card.tsx): reorder import statements for consistency and readability style(book-card.tsx): adjust class names for better formatting and maintainability

--- src/components/books/book-card.tsx
+++ src/components/books/book-card.tsx
@@ -76,9 +76,9 @@ const BookCard = ({
           </div>
           <div className="mt-2 flex min-h-[88px] flex-col gap-y-1 px-2 pb-4">
             <p className="truncate text-base font-semibold">{book.title}</p>
-            <p className="truncate font-serif text-sm italic">{book.author}</p>
+            <p className="truncate font-serif text-sm">{book.author}</p>
             {book.series && book.seriesIndex && (
-              <p className="truncate font-serif text-sm font-light italic text-muted-foreground">
+              <p className="text-muted-foreground truncate font-serif text-sm font-light italic">
                 {`${book.series} #${book.seriesIndex}`}
               </p>
             )}
