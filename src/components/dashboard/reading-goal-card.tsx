ec47856c4bd3a2308fc926e5019568365e8e69b1
Philip Steingrüber <philip.steingruber@gmail.com>
2026-01-16 23:06:13 +0100

    style(reading-goal-card.tsx): adjust font sizes and layout for better readability and alignment refactor(reading-goal-card.tsx): simplify component structure by reducing nested elements and aligning items centrally

--- src/components/dashboard/reading-goal-card.tsx
+++ src/components/dashboard/reading-goal-card.tsx
@@ -27,22 +27,20 @@ const ReadingGoalCard = ({
   return (
     <Card className={cn("h-40 w-2/5", className)}>
       <CardContent className="flex w-full justify-between">
-        <div className="mt-2 flex w-full flex-col gap-y-2">
-          <span className="w-full text-center text-2xl font-bold">{`${currentCount} of ${goal} books read this year`}</span>
-          <Progress value={progressPercentage} className="w-full" />
-          <div className="flex items-center justify-around">
-            <span className="text-xs">{`${progressPercentage}% complete`}</span>
-            <div className="flex items-center gap-x-1">
-              {isOnTrack ? (
-                <TrendingUp className="text-green-500" />
-              ) : (
-                <AlertCircle className="text-amber-500" />
-              )}
-              <span className="text-primary text-xs">{paceMessage}</span>
-            </div>
+        <div className="mt-2 flex w-full flex-col items-center gap-y-1">
+          <span className="w-full text-center text-lg font-bold">{`${currentCount} of ${goal} books read this year`}</span>
+          <Progress value={progressPercentage} className="h-4 w-3/4" />
+          <div className="flex items-center gap-x-4">
+            <span className="text-sm">{`${progressPercentage}% complete`}</span>
+            {isOnTrack ? (
+              <TrendingUp className="size-8 text-green-500" />
+            ) : (
+              <AlertCircle className="size-8 text-amber-500" />
+            )}
+            <span className="text-sm">{paceMessage}</span>
           </div>
           <div className="flex items-center justify-center gap-x-2">
-            <span className="text-xs font-semibold">Current goal: {goal}</span>
+            <span className="text-sm font-semibold">Current goal: {goal}</span>
             <Button variant={"ghost"} size={"icon"} onClick={onEditClick}>
               <EditIcon />
             </Button>
