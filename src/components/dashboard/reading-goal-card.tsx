13528a16f47a86c1a93d59939551c1d5ef84a208
Philip Steingrüber <philip.steingruber@gmail.com>
2026-01-18 22:01:48 +0100

    style(reading-goal-card.tsx): rephrase reading goal text for clarity

--- src/components/dashboard/reading-goal-card.tsx
+++ src/components/dashboard/reading-goal-card.tsx
@@ -30,7 +30,7 @@ const ReadingGoalCard = ({
     <Card className={cn("h-40 w-2/5", className)}>
       <CardContent className="flex w-full justify-between">
         <div className="mt-2 flex w-full flex-col items-center gap-y-1">
-          <span className="w-full text-center text-lg font-bold">{`${currentCount} of ${goal} (over ${threshold} pages) books read this year`}</span>
+          <span className="w-full text-center text-lg font-bold">{`${currentCount} of ${goal} books (over ${threshold} pages long) read this year`}</span>
           <Progress value={progressPercentage} className="h-4 w-3/4" />
           <div className="flex items-center gap-x-4">
             <span className="text-sm">{`${progressPercentage}% complete`}</span>
