2838b8c68ef8a83f30be3daeceb08452ab6d8d65
Philip Steingrüber <philip.steingruber@gmail.com>
2026-01-21 16:43:24 +0100

    test(use-reading-goals): remove outdated test file for useReadingGoals hook

--- src/hooks/use-reading-goals.test.tsx
+++
@@ -1,7 +0,0 @@
-import { beforeEach, describe, it, vi } from "vitest";
-
-describe("useReadingGoals", () => {
-  beforeEach(() => vi.clearAllMocks());
-
-  it("should calculate booksReadThisYear", () => {});
-});
