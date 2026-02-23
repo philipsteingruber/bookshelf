31417c7f317320b1e308bddabfd50baf7d0b96af
Philip Steingrüber <philip.steingruber@gmail.com>
2026-02-23 13:03:56 +0100

    feat(query-client.ts): enable refetchOnMount option for query client to ensure data is refreshed when component mounts

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -24,6 +24,7 @@ export function makeQueryClient(): QueryClient {
           return failureCount < 3;
         },
         refetchOnWindowFocus: true,
+        refetchOnMount: true,
       },
       dehydrate: {
         serializeData: superjson.serialize,
